cfd5eed11b2e26f5d533e81b8eabacff33a8d0f5
pschuetz <paulschuetz.mail@gmail.com>
2020-05-10 17:30:39 +0200

    update tweets based on most recent tweet stored in database

--- twitter-db-fetch/ReadMe.md
+++ twitter-db-fetch/ReadMe.md
@@ -17,5 +17,6 @@ git update-index --assume-unchanged config.py
  ## Knowledge
  - Twitter API gives us 450 requests per 15 Minutes and gives us a maximum of 100 tweets per request which makes 45000 tweets/15 minutes (https://developer.twitter.com/en/docs/tweets/search/api-reference/get-search-tweets).
  -  With the standard/free Twitter API we are able to retrieve the Tweets of the last 7 days. We can not go further in the past (https://developer.twitter.com/en/docs/tweets/search/overview).
+ - Tweets have very large identifiers (e.g. 1259491841898094592). The Cosmos DB somehow has difficulties with storing those accurately. Therefore you should work with the "id_str" and convert them to integers if you have to do numeric comparisons on identifiers
 
 
--- twitter-db-fetch/twitter_db_fetch.py
+++ twitter-db-fetch/twitter_db_fetch.py
@@ -8,16 +8,18 @@ import re
 query = 'audi etron OR audi e-tron'
 
 
-def fetch_latest_tweets(query):
-
+def fetch_latest_tweets(query, max_id):
     # url-encoded payload
     payload = {
         'q': query,
         'result_type': 'recent',
         'count': 3,
         'include_entities': 'false',
-        'tweet_mode': 'extended'
+        'tweet_mode': 'extended',
     }
+
+    if max_id is not None:
+        payload['max_id'] = max_id
 
     # headers
     bearer_token = 'Bearer ' + config.secrets["twitter-bearer-token"]
@@ -28,15 +30,22 @@ def fetch_latest_tweets(query):
         "https://api.twitter.com/1.1/search/tweets.json", params=payload, headers=headers)
 
     if r.status_code == 200:
+        # load json string into python dictionary
         data = json.loads(r.text)
-
+        # retrieve max_id for pagination
         next_results_str = data['search_metadata']['next_results']
         regex = r'max_id=(\d*)'
         max_id = int(re.findall(regex, next_results_str)[0])
+        # retrieve index of the most recent tweet fetched
+        most_recent_tweet_id = data['search_metadata']['max_id']
+
+        print('Successfully fetched ' +
+              str(len(data['statuses'])) + ' tweets.')
 
         return {
             'max_id': max_id,
-            'tweets': data['statuses']
+            'tweets': data['statuses'],
+            'most_recent_tweet_id': most_recent_tweet_id
         }
     else:
         raise Exception("Fetching twitter content did not work!" + "\n\t" + "Status: " +
@@ -50,9 +59,60 @@ def save_data_to_db(tweets):
     tweet_collection = client["coins-brand-equity-dilution-database"].tweets
 
     tweet_collection.insert_many(tweets)
+    print('Successfully saved ' + str(len(tweets)) + ' tweets to database.')
+
+
+# return -1 if there have not been stored tweets yet, otherwise the (integer) id of the latest saved tweet
+def get_latest_saved_index():
+    print('Retrieving index of most recent tweet stored in database.')
+    f = open('last_saved_tweet_index.txt', 'r')
+    index = f.readline()
+    if index is "":
+        return -1
+    else:
+        return int(index)
+
+
+def set_latest_saved_index(index):
+    print('Updating index of most recent tweet stored in database.')
+    f = open('last_saved_tweet_index.txt', 'w')
+    f.write(str(index))
+    f.close()
 
 
 # MAIN
-result = fetch_latest_tweets(query)
-tweets = result['tweets']
-save_data_to_db(tweets)
+
+# init variables
+latest_saved_index = get_latest_saved_index()
+max_id = None
+
+# if there has been a lot of tweets or we are doing an inital fetch we will at most do 50 requests a 100 tweets => saving 5000 tweets at max
+for i in range(2):
+
+    result = fetch_latest_tweets(query, max_id)
+
+    if result['most_recent_tweet_id'] == latest_saved_index:
+        print("There are no new tweets. Database is already up to date.")
+        break
+
+    # extract the id of the most recent tweet  of the first fetch to update lastest_saved_index
+    if i == 0:
+        set_latest_saved_index(result["most_recent_tweet_id"])
+
+    # if the max_id index (which determines that we want to fetch tweets older than or equal than this id) is smaller than
+    # the index of the oldest tweet saved, than we are fetching data we already have stored in our database!
+    if result["max_id"] < latest_saved_index:
+        # 1. remove all tweets <= the latest saved index
+        fetched_tweets = result["tweets"]
+        fetched_tweets_more_recent_than_last_saved = filter(
+            lambda tweet: tweet["id"] <= latest_saved_index, fetched_tweets)
+        # 2. save remaining in database
+        save_data_to_db(fetched_tweets_more_recent_than_last_saved)
+        # 3. exit for loop
+        break
+    else:
+        # just save items and set max_id for pagination
+        max_id = result["max_id"]
+        save_data_to_db(result["tweets"])
+
+    i += 1
