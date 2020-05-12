44233dea014288a9aad509022d3b46fc7910ecd5
pschuetz <paulschuetz.mail@gmail.com>
2020-05-12 13:07:03 +0200

    retrieve number of tweets stored in database

--- twitter-db-fetch/tweet_analysis.py
+++ twitter-db-fetch/tweet_analysis.py
@@ -6,8 +6,6 @@ db_connection_string = config.secrets["db-connection-string"]
 client = MongoClient(db_connection_string)
 tweet_collection = client["coins-brand-equity-dilution-database"].tweets
 
-tweets = tweet_collection.find()
-print(tweets)
-
-for tweet in tweets:
-    print(tweet['id_str'])
+tweets = list(tweet_collection.find())
+print("Currently there are " + str(len(tweets)) +
+      " tweets stored in the database.")
