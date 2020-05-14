f8ed77591c873a775054e5b0af1eeed0f7dd354d
mengels3 <mengels3@smail.uni-koeln.de>
2020-05-14 12:38:36 +0200

    more cleanup

--- twitter-db-fetch/sentiment_nltk_test.py
+++ twitter-db-fetch/sentiment_nltk_test.py
@@ -70,6 +70,25 @@ def get_custom_input():
 
 	return str(custom_input)
 
+def get_model_classifier(positive_cleaned_tokens_list, negative_cleaned_tokens_list):
+
+	positive_dataset = [(tweet_dict, "Positive") for tweet_dict in get_tweets_for_model(positive_cleaned_tokens_list)]
+	negative_dataset = [(tweet_dict, "Negative") for tweet_dict in get_tweets_for_model(negative_cleaned_tokens_list)]
+
+	dataset = positive_dataset + negative_dataset
+
+	random.shuffle(dataset)
+
+	train_data = dataset[:int((len(dataset) * 0.7))]
+	test_data = dataset[int((len(dataset) * 0.7)):]
+
+	classifier = NaiveBayesClassifier.train(train_data)
+
+	print("Accuracy is:", classify.accuracy(classifier, test_data))
+	print(classifier.show_most_informative_features(10))
+
+	return classifier
+
 def main():
 	custom_input = get_custom_input()
 
@@ -82,7 +101,6 @@ def main():
 	positive_tweet_tokens = twitter_samples.tokenized('positive_tweets.json')
 	negative_tweet_tokens = twitter_samples.tokenized('negative_tweets.json')
 
-
 	positive_cleaned_tokens_list = list()
 	negative_cleaned_tokens_list = list()
 
@@ -92,31 +110,10 @@ def main():
 	for tokens in negative_tweet_tokens:
 	    negative_cleaned_tokens_list.append(remove_noise(tokens, stop_words))
 
+	freq_dist_pos = FreqDist(get_all_words(positive_cleaned_tokens_list))
+	freq_dist_neg = FreqDist(get_all_words(negative_cleaned_tokens_list))
 
-	all_pos_words = get_all_words(positive_cleaned_tokens_list)
-	all_neg_words = get_all_words(negative_cleaned_tokens_list)
-
-	freq_dist_pos = FreqDist(all_pos_words)
-	freq_dist_neg = FreqDist(all_neg_words)
-
-	positive_tokens_for_model = get_tweets_for_model(positive_cleaned_tokens_list)
-	negative_tokens_for_model = get_tweets_for_model(negative_cleaned_tokens_list)
-
-	positive_dataset = [(tweet_dict, "Positive") for tweet_dict in positive_tokens_for_model]
-	negative_dataset = [(tweet_dict, "Negative") for tweet_dict in negative_tokens_for_model]
-
-	dataset = positive_dataset + negative_dataset
-
-	random.shuffle(dataset)
-
-	train_data = dataset[:int((len(dataset) * 0.7))]
-	test_data = dataset[int((len(dataset) * 0.7)):]
-
-	classifier = NaiveBayesClassifier.train(train_data)
-
-	print("Accuracy is:", classify.accuracy(classifier, test_data))
-
-	print(classifier.show_most_informative_features(10))
+	classifier = get_model_classifier(positive_cleaned_tokens_list, negative_cleaned_tokens_list)
 
 	print('\n\nNow your input ("%s") will be analyzed:' %custom_input)
 	custom_tokens = remove_noise(word_tokenize(custom_input))
