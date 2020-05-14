a80d44da9bd21a571ddb770c8cd7f985bb2949bf
mengels3 <mengels3@smail.uni-koeln.de>
2020-05-14 17:40:27 +0200

    enhanced sentiment analysis

--- twitter-db-fetch/sentiment_nltk_test.py
+++ twitter-db-fetch/sentiment_nltk_test.py
@@ -8,30 +8,40 @@ import random
 from nltk import FreqDist
 from nltk import classify
 from nltk import NaiveBayesClassifier
+
 from nltk.tag import pos_tag
+
 from nltk.corpus import stopwords
+from nltk.corpus import subjectivity
 from nltk.corpus import twitter_samples
+
 from nltk.tokenize import word_tokenize
+
+from nltk.sentiment import SentimentAnalyzer
+from nltk.sentiment.util import *
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+
 from nltk.stem.wordnet import WordNetLemmatizer
 
 
 
 def nltk_downloader():
-	nltk.download('twitter_samples')
-	nltk.download('punkt')
-	nltk.download('wordnet')
-	nltk.download('averaged_perceptron_tagger')
-	nltk.download('stopwords')
+    nltk.download('twitter_samples')
+    nltk.download('punkt')
+    nltk.download('wordnet')
+    nltk.download('averaged_perceptron_tagger')
+    nltk.download('stopwords')
+    nltk.download('vader_lexicon')
 
 def fetch_twitter_samples():
 
-	positive_tweets = twitter_samples.strings('positive_tweets.json')
-	negative_tweets = twitter_samples.strings('negative_tweets.json')
-	text = twitter_samples.strings('tweets.20150430-223406.json')
+    positive_tweets = twitter_samples.strings('positive_tweets.json')
+    negative_tweets = twitter_samples.strings('negative_tweets.json')
+    text = twitter_samples.strings('tweets.20150430-223406.json')
 
-	return positive_tweets, negative_tweets, text
+    return positive_tweets, negative_tweets, text
 
-def remove_noise(tweet_tokens, stop_words = ()):
+def remove_noise(tweet_tokens, stop_words):
 
     cleaned_tokens = []
 
@@ -64,60 +74,85 @@ def get_tweets_for_model(cleaned_tokens_list):
         yield dict([token, True] for token in tweet_tokens)
 
 def get_custom_input():
-	print('Type your Custom Text to be analyzed and press Enter:')
-	custom_input = input()
-	print('Thanks. Your Text is "%s".\nThe Model will be trained first, afterwards your input will be analyzed. This could take some time.' %custom_input)
+    print('Type your Custom Text to be analyzed and press Enter:')
+    custom_input = input()
+    # custom_input = "Many positive vibes. Yeah :)"
+    print('Thanks. Your Text is "%s".\nThe Model will be trained first, afterwards your input will be analyzed. This could take some time.' %custom_input)
 
-	return str(custom_input)
+    return str(custom_input)
 
-def get_model_classifier(positive_cleaned_tokens_list, negative_cleaned_tokens_list):
+def get_train_test_data(positive_cleaned_tokens_list, negative_cleaned_tokens_list):
 
-	positive_dataset = [(tweet_dict, "Positive") for tweet_dict in get_tweets_for_model(positive_cleaned_tokens_list)]
-	negative_dataset = [(tweet_dict, "Negative") for tweet_dict in get_tweets_for_model(negative_cleaned_tokens_list)]
+    positive_dataset = [(tweet_dict, "Positive") for tweet_dict in get_tweets_for_model(positive_cleaned_tokens_list)]
+    negative_dataset = [(tweet_dict, "Negative") for tweet_dict in get_tweets_for_model(negative_cleaned_tokens_list)]
 
-	dataset = positive_dataset + negative_dataset
+    dataset = positive_dataset + negative_dataset
 
-	random.shuffle(dataset)
+    random.shuffle(dataset)
 
-	train_data = dataset[:int((len(dataset) * 0.7))]
-	test_data = dataset[int((len(dataset) * 0.7)):]
+    train_data = dataset[:int((len(dataset) * 0.7))]
+    test_data = dataset[int((len(dataset) * 0.7)):]
 
-	classifier = NaiveBayesClassifier.train(train_data)
+    return train_data, test_data
 
-	print("Accuracy is:", classify.accuracy(classifier, test_data))
-	print(classifier.show_most_informative_features(10))
 
-	return classifier
+def get_model_classifier(train_data, test_data, classifier_type):
+
+    classifier = classifier_type.train(train_data)
+
+    print("Accuracy is:", classify.accuracy(classifier, test_data))
+    print(classifier.show_most_informative_features(10))
+
+    return classifier
 
 def main():
-	custom_input = get_custom_input()
+    custom_input = get_custom_input()
+    stop_words = stopwords.words('english')
+    custom_tokens = remove_noise(word_tokenize(custom_input), stop_words)
 
-	nltk_downloader()
-	
-	stop_words = stopwords.words('english')
+    nltk_downloader()
+    
+    sid = SentimentIntensityAnalyzer()
 
-	positive_tweets, negative_tweets, text = fetch_twitter_samples()
+    print("")
+    print("1: Sentiment Intensity Analysis: poitive, negative, neutral percentages")
+    print("Sentiment Intensity without noise removal")
+    ss = sid.polarity_scores(custom_input)
+    for k in sorted(ss):
+        print('{0}: {1}, '.format(k, ss[k]), end='')
+    print()
 
-	positive_tweet_tokens = twitter_samples.tokenized('positive_tweets.json')
-	negative_tweet_tokens = twitter_samples.tokenized('negative_tweets.json')
+    print("Sentiment Intensity with noise removal")
+    ss = sid.polarity_scores(" ".join(custom_tokens))
+    for k in sorted(ss):
+        print('{0}: {1}, '.format(k, ss[k]), end='')
+    print()
 
-	positive_cleaned_tokens_list = list()
-	negative_cleaned_tokens_list = list()
+    print("")
+    print('2: Basic Sentiment Analysis: Only Positive or Negative')
+    positive_tweets, negative_tweets, text = fetch_twitter_samples()
 
-	for tokens in positive_tweet_tokens:
-	    positive_cleaned_tokens_list.append(remove_noise(tokens, stop_words))
+    positive_tweet_tokens = twitter_samples.tokenized('positive_tweets.json')
+    negative_tweet_tokens = twitter_samples.tokenized('negative_tweets.json')
 
-	for tokens in negative_tweet_tokens:
-	    negative_cleaned_tokens_list.append(remove_noise(tokens, stop_words))
+    positive_cleaned_tokens_list = list()
+    negative_cleaned_tokens_list = list()
 
-	freq_dist_pos = FreqDist(get_all_words(positive_cleaned_tokens_list))
-	freq_dist_neg = FreqDist(get_all_words(negative_cleaned_tokens_list))
+    for tokens in positive_tweet_tokens:
+        positive_cleaned_tokens_list.append(remove_noise(tokens, stop_words))
 
-	classifier = get_model_classifier(positive_cleaned_tokens_list, negative_cleaned_tokens_list)
+    for tokens in negative_tweet_tokens:
+        negative_cleaned_tokens_list.append(remove_noise(tokens, stop_words))
 
-	print('\n\nNow your input ("%s") will be analyzed:' %custom_input)
-	custom_tokens = remove_noise(word_tokenize(custom_input))
-	print("Result: ", classifier.classify(dict([token, True] for token in custom_tokens)))
+    freq_dist_pos = FreqDist(get_all_words(positive_cleaned_tokens_list))
+    freq_dist_neg = FreqDist(get_all_words(negative_cleaned_tokens_list))
+
+    train_data, test_data = get_train_test_data(positive_cleaned_tokens_list, negative_cleaned_tokens_list)
+
+    classifier = get_model_classifier(train_data, test_data, NaiveBayesClassifier)
+
+    print("Result: ", classifier.classify(dict([token, True] for token in custom_tokens)))
+
 
 if __name__ == '__main__':
-	main()
+    main()
