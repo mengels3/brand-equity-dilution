c0b19140aa94a39cac4477a088da60ab166b746d
pschuetz <paulschuetz.mail@gmail.com>
2020-06-23 16:41:38 +0200

    brand extension fit calculation

--- twitter-db-fetch/brand-extension-fit-calculation.py
+++ twitter-db-fetch/brand-extension-fit-calculation.py
@@ -6,38 +6,8 @@ from nltk.probability import FreqDist
 import matplotlib.pyplot as plt
 import string
 import re
+from collections import Counter
 
-
-# sentence = "@potheadphrog777 @shaken_pie @Linked_Caleb Random guy in Audi.\nI tend to believe him 😂 https://t.co/K0pedGq7Vc"
-# # if retweet remove 'RT' at beginning
-# if sentence.startswith('RT'):
-#     sentence = sentence[2:]
-# # remove line breaks
-# sentence = re.sub(r'[\n]+', ' ', sentence)
-# # remove all @-handles such as @sam_harris
-# sentence = re.sub(r'(@[^\s]+)', '', sentence)
-# # remove all non alphanumeric chars
-# sentence = re.sub(r'[^A-Za-z0-9 ]+', '', sentence)
-# # remove all leading and trainling whitespace or tabs
-# sentence = sentence.strip()
-# print('cleaned: ' + sentence)
-
-# # POS tagging and filtering for nouns
-# tokens = nltk.word_tokenize(sentence.lower())
-# tokens = list(filter(lambda token: not str(token).startswith('http'), tokens))
-# print('tokens: ' + str(tokens))
-# tagged = nltk.pos_tag(tokens)
-# print('pos_tagged: ' + str(tagged))
-# nouns = list(filter(lambda taggedWord: taggedWord[1] == 'NN' or taggedWord[1]
-#                     == 'NNS' or taggedWord[1] == 'NNP' or taggedWord[1] == 'NNPS', tagged))
-# nouns = list(map(lambda noun: noun[0], nouns))
-# stop_words = set(stopwords.words('english'))
-# filtered_nouns = [w for w in nouns if not w in stop_words]
-
-# lem = WordNetLemmatizer()
-# lemmatized_nouns = list(
-#     map(lambda noun: lem.lemmatize(noun), filtered_nouns))
-# print('lemmatized_nouns: ' + str(lemmatized_nouns))
 
 def calculateTermFrequency(dbClient, collection):
     # 1. get all tweets
@@ -57,8 +27,12 @@ def calculateTermFrequency(dbClient, collection):
         sentence = re.sub(r'[\n]+', ' ', sentence)
         # remove all @-handles such as @sam_harris
         sentence = re.sub(r'(@[^\s]+)', '', sentence)
+        # remove all .com links beacause twitter images tend not to specify https which undergoes our link filter
+        sentence = re.sub(r'(\S*\.com\/*\S*)', ' ', sentence)
+        # collapse all "-"
+        sentence = re.sub('-', '', sentence)
         # remove all non alphanumeric chars
-        sentence = re.sub(r'[^A-Za-z0-9 ]+', '', sentence)
+        sentence = re.sub(r'[^A-Za-z0-9]+', ' ', sentence)
         # remove all leading and trainling whitespace or tabs
         sentence = sentence.strip()
 
@@ -79,6 +53,11 @@ def calculateTermFrequency(dbClient, collection):
         # print('lemmatized_nouns: ' + str(lemmatized_nouns))
 
         updateOccurences(occurences, lemmatized_nouns)
+
+    # calculate relative occurence frequency
+    totalNounOccurences = sum(occurences.values())
+    for key in occurences:
+        occurences[key] = occurences[key] / totalNounOccurences
     return occurences
 
 
@@ -90,13 +69,42 @@ def updateOccurences(dictionary, occurences):
             dictionary[occurence] = 1
 
 
+def calculateMeanFractionalDifference(termFreqMain, termFreqExtension):
+    top50main = Counter(termFreqMain).most_common(50)
+
+    fractionSum = 0
+    for term in top50main:
+        scoreInExtension = 0
+        if term[0] in termFreqExtension:
+            scoreInExtension = termFreqExtension[term[0]]
+        fraction = 0
+        if term[1] <= scoreInExtension:
+            fraction = term[1] / scoreInExtension
+            print('fractional difference for ' + term[0] + ' is: ' + str(
+                term[1]) + ' : ' + str(scoreInExtension) + ' = ' + str(fraction))
+        else:
+            fraction = scoreInExtension / term[1]
+            print('fractional difference for ' + term[0] + ' is: ' + str(
+                scoreInExtension) + ' : ' + str(term[1]) + ' = ' + str(fraction))
+        fractionSum += fraction
+
+    meanDifference = fractionSum / 50
+    return meanDifference
+
+
 dbClient = DatabaseClient()
 termFrequencyAudi = calculateTermFrequency(dbClient, 'audi')
 termFrequencyAudi.pop('audi', None)
+fDist = FreqDist(termFrequencyAudi)
+fDist.plot(35)
 
 termFrequencyAudiEtron = calculateTermFrequency(dbClient, 'audi_etron')
 termFrequencyAudiEtron.pop('audi', None)
 termFrequencyAudiEtron.pop('etron', None)
-# fDist = FreqDist(termFrequencyAudi)
-# fDist.plot(10)
-# fDist.plot(20)
+fDist = FreqDist(termFrequencyAudiEtron)
+fDist.plot(35)
+
+meanDifference = calculateMeanFractionalDifference(
+    termFrequencyAudi, termFrequencyAudiEtron)
+print('Mean difference in frequency as measure for product fit: ' +
+      str(meanDifference))
