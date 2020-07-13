10231eac7b37d1ec39cde98ca30feb4be5d19e24
mengels3 <mengels3@smail.uni-koeln.de>
2020-07-13 18:08:19 +0200

    db client

--- sentiment-analysis/database_client.py
+++ sentiment-analysis/database_client.py
@@ -1,6 +1,7 @@
 from pymongo import MongoClient
 import pymongo
 import config
+import time
 
 
 class DatabaseClient:
@@ -8,11 +9,26 @@ class DatabaseClient:
         db_connection_string = config.secrets["db-connection-string"]
         self.client = MongoClient(db_connection_string)
         self.database = self.client["coins-brand-equity-dilution-database"]
+        print('Initalized database client.')
 
-    def getLatestTweetId(self, collection_name):
-        tweets = list(self.database[collection_name].find())
+    def getLatestTweetId(self, collection_name, retries, timeoutSec):
+        tweets = None
+        for i in range(retries):
+            try:
+                collection = self.database[collection_name]
+                tweets = list(collection.find({}, projection={'id_str': 1}))
+                break
+            except Exception as e:
+                if i == retries-1:
+                    raise e
+                print(
+                    'There was an error with reading from database (try ' + str(i+1) + '/' + str(retries) + '): ' + str(e))
+                print('Sleep for ' + str(timeoutSec) +
+                      ' seconds and try again.')
+                time.sleep(timeoutSec)
 
         if len(tweets) == 0:
+            print("No tweets stored in collection " + collection_name)
             return -1
 
         max_id = max(list(map(lambda tweet: int(tweet['id_str']), tweets)))
@@ -23,7 +39,25 @@ class DatabaseClient:
     def getAllDocuments(self, collection_name):
         return list(self.database[collection_name].find())
 
+    def getAllDocumentsInChunks(self, collection_name, chunk_size, timeoutSec):
+
+        results = []
+        i = 0
+        while True:
+            cursor = self.database[collection_name].find(
+                {}).skip(len(results)).limit(chunk_size)
+            newDocs = list(cursor)
+            cursor.close()
+            if len(newDocs) == 0:
+                break
+            results.extend(newDocs)
+            print(len(results))
+            i += 1
+            time.sleep(timeoutSec)
+        return results
+
     def saveDocuments(self, collection_name, documents):
-        self.database[collection_name].insert_many(documents)
+        collection = self.database[collection_name]
+        collection.insert_many(documents)
         print('Successfully saved ' + str(len(documents)) +
               ' documents to database ' + collection_name + '.')
